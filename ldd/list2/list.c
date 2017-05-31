ecd9fd2a2b1149d90fd3346fcb64809c92e11018
jia <1730548875@qq.com>
2017-05-31 08:13:38 +0800

    list--have a bug

--- ldd/list2/list.c
+++ ldd/list2/list.c
@@ -1,7 +1,12 @@
+/*
+  练习内核list的使用。
+  要求：列表按注册时的名字排列。
+*/
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/list.h>
-
+#include <linux/slab.h>
+#include <linux/string.h>
 struct usb_info{
 	int pid;
 	char *name;
@@ -12,7 +17,21 @@ struct usb_info *info;
 
 int register_usb(int pid,char *name)
 {
-	
+  struct usb_info *p;
+
+  p=kmalloc(sizeof(struct usb_info),GFP_KERNEL);
+
+  if(p == NULL)
+    return -ENOMEM;
+
+  p->pid=pid;
+  p->name=kstrdup(name,GFP_KERNEL);
+
+  if(p->name == NULL)
+    return -ENOMEM;
+  
+  list_add(p,info);
+  return 0;
 }
 
 MODULE_LICENSE("GPL");
@@ -20,6 +39,10 @@ MODULE_LICENSE("GPL");
 static int list_init(void)
 {
 	printk(KERN_ALERT"list world!\n");
+	register_usb(1,"usb_1");
+	register_usb(2,"usb_2");
+	register_usb(3,"usb_3");
+	
 	return 0;
 }
 
