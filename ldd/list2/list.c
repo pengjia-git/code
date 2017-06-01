38f5372265704cedc004a30edfe13189b4e53de1
jia <1730548875@qq.com>
2017-06-01 22:51:00 +0800

    list 5

--- ldd/list2/list.c
+++ ldd/list2/list.c
@@ -18,7 +18,7 @@ static LIST_HEAD(usb_list);
 int register_usb(int pid,char *name)
 {
   struct usb_info *p,*pos;
-  struct list_head *insert_pos;
+
   p=kmalloc(sizeof(struct usb_info),GFP_KERNEL);
 
   if(p == NULL)
@@ -30,51 +30,46 @@ int register_usb(int pid,char *name)
   if(p->name == NULL)
     return -ENOMEM;
   if(list_empty(&usb_list)){
-    printk(KERN_ALERT"list is emtpy\n");
     list_add(&p->list,&usb_list);
     return 0;
   }
-  printk(KERN_ALERT"++++\n");
-  list_for_each_entry(pos,&usb_list,list){
-    printk(KERN_ALERT"pos->name=%s,p->name=%s\n",pos->name,p->name);
-    if(pos->name && (strcmp(p->name,pos->name) <0 )){
-      printk(KERN_ALERT"cannont go here\n");
 
-      break;
+  list_for_each_entry(pos,&usb_list,list){
+    if(pos->name && (strcmp(p->name,pos->name) <0 )){
+      list_add_tail(&p->list,&(pos->list));
+      return 0;
     }
   }
-  printk(KERN_ALERT"---------------\n");
-      list_prev_entry(pos,list);
-  printk(KERN_ALERT"pos->name=%s\n",pos->name);
-  list_add(&p->list,&pos->list);
+  list_add_tail(&p->list,&usb_list);
   return 0;
 }
 
 void print_list(struct list_head *p)
 {
   struct usb_info *pos;
-  //printk(KERN_ALERT"%d %s\n",p->pid,p->name);
   list_for_each_entry(pos,p,list){
     printk(KERN_ALERT"%d %s\n",pos->pid,pos->name);
-  }
-	
+  }	
 }
-MODULE_LICENSE("GPL");
+
 
 static int list_init(void)
 {
-  register_usb(1,"usb_1");
-  register_usb(2,"usb_2");
-  register_usb(3,"usb_3");
-  register_usb(4,"usb_4");
+  register_usb(2,"uvc");
+  register_usb(4,"usb");
+  register_usb(3,"adb");
+  register_usb(1,"mnt");
+
   print_list(&usb_list);
+
   return 0;
 }
 
-static int list_exit(void)
+static void list_exit(void)
 {
-  return 0;
 }
 
+MODULE_LICENSE("GPL");
 module_init(list_init);
 module_exit(list_exit);
+
