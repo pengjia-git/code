aa525c56883b2043e6cbc9b17e3d6a4dc96b7d77
jia <1730548875@qq.com>
2017-08-30 21:21:38 +0800

    scanner and pow

---
+++ java/ComputeSum/ComputeSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ComputeSum{
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		double num1=input.nextDouble();
+		double num2=input.nextByte();
+		double num3=input.nextDouble();
+		double sum=num1+num2+num3;
+	System.out.println("sum=" + sum);
+	
+	}
+	
+}
---
+++ java/Pow/Pow.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Pow{
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		double a=input.nextDouble();
+		double b=input.nextDouble();
+		System.out.println("a^b= "+Math.pow(a,b));
+	}
+}
