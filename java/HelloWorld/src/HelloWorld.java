fc3a5d3afa42f1b8bece479b5e52c04d2edb207d
pengjia <1730548875@qq.com>
2017-08-30 23:15:30 +0800

    用eclipse 建立了一个helloworld工程

---
+++ java/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author 17305
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
