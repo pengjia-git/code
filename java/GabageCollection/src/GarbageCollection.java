382eb881fdd3d8abdf79784d3d3c99a2270286d6
jia <1730548875@qq.com>
2017-09-03 23:18:34 +0800

    GarbageCollection

---
+++ java/GabageCollection/src/GarbageCollection.java
@@ -0,0 +1,12 @@
+
+public class GarbageCollection {
+
+	public static void main(String[] args) {
+		//首先，语法上就错了。
+		double[] arr10=new double[40];
+		double[] arr10=new double[20];
+		arr10[38]=1;
+
+	}
+
+}
