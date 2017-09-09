1b82039eadf8b49cbc7b79acdf3c014327d1cb6e
pengjia <1730548875@qq.com>
2017-09-09 20:47:47 +0800

    DynamicBindingDemo

---
+++ java/DynamicBindingDemo/src/DynamicBindingDemo.java
@@ -0,0 +1,31 @@
+
+public class DynamicBindingDemo {
+
+	public static void main(String[] args) {
+		//m(new GraduateStudent());
+		//m(new Student());
+		//m(new Person());
+		m(new DynamicBindingDemo());
+	}
+
+	public static void m(Object x) {
+		System.out.println(x.toString());
+	}
+}
+
+class GraduateStudent extends Student{
+	
+}
+
+class Student extends Person{
+	@Override
+	public String toString() {
+		return "Student";
+	}
+}
+
+class Person {
+	public String toString() {
+		return "Person";
+	}
+}
