23b788ee7df1eef840f61c5ab269f66abe9c2a24
pengjia <1730548875@qq.com>
2017-11-19 15:52:51 +0800

    uicostom

---
+++ Android_learning/UICustomViews/app/src/main/java/com/example/uicustomviews/TitleLayout.java
@@ -0,0 +1,27 @@
+package com.example.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 17305 on 2017/11/19.
+ */
+public  class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context, AttributeSet attrs){
+        super(context,attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button titleBack=(Button)findViewById(R.id.title_back);
+        Button titleEdit=(Button)findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity)getContext()).finish();
+            }
+        });
+    }
+}
